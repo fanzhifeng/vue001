371674e79f4648275ee2ca5bb50dd5e459671381
fanfan <fanfan@qq.com>
2019-07-26 15:15:54 +0800

    init my priject

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// 入口文件
+import Vue from 'vue'
+
+// 导入 MUI的样式
+import './lib/mui/css/mui.min.css'
+
+import { Header } from 'mint-ui';
+
+Vue.component(Header.name, Header);
+
+// 导入App根组件
+import app from './App.vue'
+
+
+
+var vm = new Vue({
+  el: '#app',
+  render: c => c(app)
+})
+
